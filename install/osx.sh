731c92d112ae77f911a88327f3c74a1f0f48e883
narcissus <1044532074@qq.com>
2023-06-16 14:46:03 +0800

    [feature] osx finder config.

--- install/osx.sh
+++ install/osx.sh
@@ -1,8 +1,6 @@
 #!/bin/zsh
 
 # Dock
-echo "\033[32mSet Dock.\033[0m"
-
 echo "Automatically hide the Dock"
 defaults write com.apple.dock "autohide" -bool "true"
 
@@ -14,3 +12,14 @@ defaults write com.apple.dock "show-recents" -bool "false"
 
 echo "Close rearrange automatically"
 defaults write com.apple.dock "mru-spaces" -bool "false"
+
+killall Dock
+
+# Finder
+echo "Open path bar"
+defaults write com.apple.finder ShowPathbar -bool "true"
+
+echo "Automatically empty bin after 30 days"
+defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true"
+
+killall Finder
