bff264fa8929efb37b0494588fca28fc87ad011b
dengzhicheng03 <dengzhicheng03@meituan.com>
2026-05-26 11:10:04 +0800

    add full border plugin

--- yazi/init.lua
+++ yazi/init.lua
@@ -3,3 +3,9 @@ require("git"):setup {
 	-- Order of status signs showing in the linemode
 	order = 1500,
 }
+
+-- full border
+require("full-border"):setup {
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+}
